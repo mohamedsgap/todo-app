d87fc74c4adc064809fbc89386fab71889535fc0
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-12-02 10:26:25 +0200

    feat: track checked & unchecked items

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let itemCount = 0 ;
+let uncheckedCount = 0;
+
+function updateItemCount(diff) {
+  itemCount += diff ;
+  itemCountSpan.innerHTML= itemCount;
+}
+
+function updateUncheckedCount (diff) {
+  uncheckedCount -= diff ;
+  uncheckedCountSpan.innerHTML(uncheckedCount);
+}
+
+
 function newTodo() {
   alert('New TODO button clicked!')
 }
