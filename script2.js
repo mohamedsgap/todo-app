48139f91ef00c3de5aa6007ba223fe9aad328f6c
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-12-02 11:36:11 +0200

    feat: Todo

--- script2.js
+++ script2.js
@@ -3,17 +3,34 @@ const classNames = {
     TODO_ITEM: 'todo-container',
     TODO_CHECKBOX: 'todo-checkbox',
     TODO_TEXT: 'todo-text',
-  }
-  
-  const list = document.getElementById('todo-list');
-  const itemCountDiv = document.getElementById('item-count');
-  const uncheckedCountDiv = document.getElementById('unchecked-count');
+}
+
+const list = document.getElementById('todo-list');
+const itemCountDiv = document.getElementById('item-count');
+const uncheckedCountDiv = document.getElementById('unchecked-count');
 
 
-  let todos = [];
+let todos = [];
 
-  function count(arr, func){
-      return arr.reduce((acc, next) => func(next) ? acc +1 : acc , 0); 
-  }
+function count(arr, func) {
+    return arr.reduce((acc, next) => func(next) ? acc + 1 : acc, 0);
+}
+
+function Todo(name) {
+    this.name = name || 'New TODO'
+    this.checkbox = null
+    this.element = null
+    this.checked = false
+}
+
+Todo.prototype.toggleCheck = function () {
+    this.checked = !this.checked
+    if (this.element && this.checked) {
+        this.checkbox.checked = this.checked;
+    } else {
+        this.element = renderTodo(this);
+    }
+}
+
 
 
