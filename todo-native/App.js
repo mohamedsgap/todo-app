824489f122950f5a3062a10ce389aceb0ebd5dee
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-12-07 23:39:48 +0200

    import more stuff

--- todo-native/App.js
+++ todo-native/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch, ScrollView, Button } from 'react-native';
+import {Constants} from 'expo'
 
 export default function App() {
   return (
